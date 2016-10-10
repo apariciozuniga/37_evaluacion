9ed3198edd1e1c183a36cd9c0df4ce8e1f0145ec
apariciozuniga <bereniceaparicio31@gmail.com>
2016-10-10 18:07:38 +0000

    evaluacion de javascript

---
+++ ejercicio2.js
@@ -0,0 +1,31 @@
+
+/*Ejercicio 2: Declara un arreglo vacío denominado nombres.
+ Pide al usuario tres nombres y almacena esos nombres como elementos arreglo. 
+ A continuación muestra el contenido en un mensaje.
+
+PSEUDOCODIGO
+
+INICIO
+array () nombres
+escriba un nombre;
+Leer nombreA;
+escribe otro nombre;
+Leer nombreB;
+escriba Dame otro nombre;
+Leer nombreC;
+var nombre_array = ["nombreA", "nombreB", "nombreC"];
+Muestre contenido de nombres;
+
+FIN
+
+*/
+
+var nombreA = prompt("Dame un nombre");
+var nombreA = nombreA;
+var nombreB = prompt("Dame otro nombre");
+var nombreB = nombreB;
+var nombreC = prompt("Uno más");
+var nombreC = nombreC;
+var nombres = [nombreA, nombreB, nombreC]
+document.write("tus nombres son: " + nombres);
+
